e59223aa5b3d3d8bad07deb9e447d098668ef145
srdjan <srdjan.gr@gmail.com>
2023-07-04 18:17:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div>
+      cioa
+    </div>
+  )
+}
+
+export default App
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    fonts: {
+        // heading: `'Open Sans', sans-serif`,
+        body: `'Ubuntu', sans-serif`,
+    },
+    letterSpacings: {
+        tighter: "-0.05em",
+        tight: "-0.025em",
+        normal: "0",
+        wide: "0.025em",
+        wider: "0.05em",
+        widest: "0.1em",
+    },
+    // breakpoints: {
+    //   sm: '30em', // 480px
+    //   md: '48em', // 768px
+    //   lg: '62em', // 992px
+    //   xl: '80em', // 1280px
+    //   '2xl': '96em', // 1536px
+    // }
+})
+
+export default theme
